c644b24a6e7f4e334674b1d6d346bbe4b354c3b9
황정호 <jhhwang@selab.co.kr>
2019-01-31 18:58:31 +0900

    두번쨰커밋 오늘은 다하자

--- socket.js
+++ socket.js
@@ -6,11 +6,27 @@ module.exports = (server,app,sessionMiddleware) => {
     const io = SocketIO(server,{path : '/socket.io'}); //받은 서버 소켓서버로 업글
 
     app.set('io',io);//app 에서 io 사용하려고 set 했음
+    const room = io.of('/room');
+    const chat = io.of('/chat');
 
-    io.on('connection',socket => {
-        console.log('socket 연결함');
+    io.use((socket,next) => {
+        sessionMiddleware(socket.request,socket.request.res,next);
     });
 
+    room.on('connection',socket => {
+        console.log('room socket 연결');
+        socket.on('disconnect', () => {
+            console.log('room socket 끝');
+        });
+    });
+
+    chat.on('connection',socket => {
+        console.log('chat socket 연결');
+        socket.on('disconnect',() => {
+            console.log('chat socket 끝');
+        });
+    });
+    
 
 
 }
